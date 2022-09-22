f2d6b3ea9bc6b8bd3d690b0c40e438b2458dcb27
Harshana24 <68814311+Harshana24@users.noreply.github.com>
2022-09-22 12:20:10 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Palindrome
+{
+public static void main(String args[])
+{
+Scanner in = new Scanner(System.in);
+System.out.println("George says:");
+int n = in.nextInt();
+int sum = 0, r;
+int temp = n;
+
+if(n>-1)
+{
+while(n>0)
+{
+r = n % 10;
+sum = (sum*10)+r;
+n = n/10;
+}
+System.out.println("Tintin says:");
+if(temp==sum)
+System.out.println("Palindrome");
+else
+System.out.println("Not a Palindrome");
+}
+else
+{
+System.out.println("Invalid Input");
+}
+}
+}
