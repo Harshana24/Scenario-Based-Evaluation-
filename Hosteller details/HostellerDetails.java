a3c5addb3fa8d94c9d4865582c1c6ae41702310f
Harshana24 <68814311+Harshana24@users.noreply.github.com>
2022-09-22 16:29:04 +0530

    Create HostellerDetails.java

---
+++ Hosteller details/HostellerDetails.java
@@ -0,0 +1,20 @@
+public class HostellerDetails extends StudentDetails {
+    private String hostelName;
+    private int roomNumber;
+
+    public String getHostelName() {
+        return hostelName;
+    }
+
+    public void setHostelName(String hostelName) {
+        this.hostelName = hostelName;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+}
